7a013c085e483ef3f9187d38d1fa81036f07a666
snowbadge2 <146172569+snowbadge2@users.noreply.github.com>
2023-10-04 10:14:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Moms New Healthy Dinner')
 streamlit.header('Breakfast Menu')
